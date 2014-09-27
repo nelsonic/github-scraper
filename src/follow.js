c17ffc0482b41eadb358402f48185575fde481d0
nelsonic <contact.nelsonic@gmail.com>
2014-09-27 17:43:38 +0100

    scrapes pages of followers

---
+++ src/follow.js
@@ -0,0 +1,61 @@
+var request = require('request');
+var cheerio = require('cheerio');
+
+// basic info
+function generic(method, user, page, f, callback) {
+  // console.log(user + ' - - - - Page:' + page + ' - - - - ' + f.length);
+  var url = 'https://github.com/' + user + '/' + method + '?page=' + page;
+
+  request(url, function (error, response, html) {
+    if (!error && response.statusCode === 200) {
+
+      var $ = cheerio.load(html);
+
+      $('.follow-list-item').each(function () {
+        f.push($(this).find('a').attr('href').replace("/", ""));
+      });
+
+      // check pagination has a "next" button indicating more people
+      // borrowed from: http://stackoverflow.com/questions/6673777/
+      var nextpage = $('.pagination a').filter(function () { 
+        return $(this).text() === "Next"; 
+      });
+
+      if (typeof nextpage !==  'undefined' && nextpage.length !== 0) {
+        // e.g: https://github.com/andrew/following?page=3
+        page = nextpage.attr('href').split("=")[1];
+        generic(method, user, page, f, callback); // recursive
+      } else {
+        callback(null, f);
+      }
+
+    } else {
+      callback(response.statusCode);
+    }
+  });
+}
+
+function followers(user, callback) {
+  generic('followers', user, 1, [], callback);
+}
+
+function following(user, callback) {
+  generic('following', user, 1, [], callback);
+}
+
+module.exports = {
+  following: following,
+  followers: followers
+}
+
+// following('andrew', function (err, f) {
+//   console.log("Totall Following: " + f.length);
+// });
+
+// following('sabariz', function (err, f) {
+//   console.log("Totall Following: " + f.length);
+// });
+
+// following('csjaba', function (err, f) {
+//   console.log("Totall Following: " + f.length);
+// });
--- test/followers.js
+++ test/followers.js
@@ -2,29 +2,29 @@
 "use strict";
 
 var test = require('tape');
-var scrape = require('../src/follow.js');
+var f = require('../src/follow.js');
 
 // test for failure (user doesn't exist HTTP status = 404)
 test('Non-existant user doesnt have a following page', function (assert) {
 	var user = Math.floor(Math.random() * 1000000000000000); // a nice long "random" number
-	scrape.following(user, function (err, s) {
+	f.followers(user, function (err, s) {
 		assert.ok(err === 404, '- 404 unknown user @' + user + ' no following page');
 		assert.end();
 	});
 });
 
-test('Linus Torvals is not following anyone', function (assert) {
-	var user = 'torvalds';
-	scrape.following(user, function (err, s) {
+test('@Zero has zero followers', function (assert) {
+	var user = 'zero';
+	f.followers(user, function (err, s) {
 		assert.ok(s.length === 0, '- @' + user + ' doesnt follow.');
 		assert.end();
 	});
 });
 
-test('Chris is Following more than 51 (1 page of) people', function (assert) {
-	var user = 'cwaring';
-	scrape.following(user, function (err, s) {
-		assert.ok(s.length > 51, '- @' + user + ' is Following ' + s.length + ' people');
+test('Alan has more than 51 (1 page of) followers', function (assert) {
+	var user = 'alanshaw';
+	f.followers(user, function (err, s) {
+		assert.ok(s.length > 51, '- @' + user + ' has ' + s.length + ' followers');
 		assert.end();
 	});
 });
