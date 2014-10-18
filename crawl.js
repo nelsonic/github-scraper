59bae315d57871a31ce138a1c1b2f701e1dea040
nelsonic <contact.nelsonic@gmail.com>
2014-10-18 09:48:37 +0100

    [WiP] check last time a user`s profile was last crawled

--- crawl.js
+++ crawl.js
@@ -1,22 +1,10 @@
 var P = require('./src/profile.js');
 var F = require('./src/follow.js');
 var fs = require('fs');
+var path = require('path');
 var users = [];
 
 var limit = 10; // concurrency limit
-
-function checkLastCrawled(user, callback){
-  var filename = './data/'+user+'.log';
-  // check when the last time we crawled a user profile was
-  // by reading the mtime on the file
-  fs.fstat(filename, function(err, stats){
-    if(err) { // file doesn't exist, run crawl!
-      callback();
-    }
-    console.log(stats);
-  })
-
-}
 
 function cleanup(user) {
   // remove dupes from list of users
@@ -32,7 +20,6 @@ function cleanup(user) {
 }
 
 function crawl(user) {
-
   var filename = './data/'+user+'.log';
 
   P.profile(user, function(error, profile){
@@ -66,7 +53,40 @@ function crawl(user) {
   })
 }
 
+function checkLastCrawled(user){
+  var filename = path.normalize('./data/'+user+'.log');
+  console.log(filename, __filename);
+  // check when the last time we crawled a user profile was
+  // by reading the mtime on the file
+  fs.stat(filename, function(err, stats){
+    if(err) { // file doesn't exist, run crawl!
+      crawl(user);
+    }
+    console.log(stats.mtime);
+    // only check every 24 hours
+    var mtime = new Date(stats.mtime);
+    var now = new Date();
+    var diff = Number(now.getTime()-mtime.getTime());
+    console.log(now + ' - ' + mtime + ' = '+diff );
+
+    // if the difference is bigger than 24 hours crawl again.
+    if(diff > 24 * 3600 * 1000) {
+      crawl(user);
+    } else {
+      cleanup(user); // remove user from users array (dont re-crawl)
+      return crawl(users[0]); // next iteration
+    }
+  })
+}
+
+checkLastCrawled('alanshaw', function(){
+  console.log('done');
+})
+
+// function keep going
+
+
 
 // Patient Zero
 var user = 'alanshaw';
-crawl(user);
+// crawl(user);
