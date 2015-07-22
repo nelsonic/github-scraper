1ed17837f857b450aa617b450913a4293a6b98a3
Nelson <contact.nelsonic@gmail.com>
2015-07-23 00:11:40 +0100

    adds fialing test for RSS feed reader/parser

---
+++ test/feed.test.js
@@ -0,0 +1,19 @@
+var test = require('tape');
+var feed = require('../lib/feed');
+
+test('expect 400 when username not stated', function(t){
+  var user = '';
+  feed(user, function(err, repos){
+    t.ok(err === 400, 'got 400 error when no user defined');
+    t.end();
+  })
+})
+
+test('expect random (non-existent) user to return 404 error ', function(t){
+	var user = Math.floor(Math.random() * 1000000000000000); // a nice long "random" number
+	feed(user, function(err, repos){
+		t.ok(err === 404, 'Got 404 Error when username does not exist');
+		t.ok(typeof repos === 'undefined', '@param repos is undefined (as expected)');
+		t.end();
+	})
+})
