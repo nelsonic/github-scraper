82ffa761bb0daddbec0c4474504d5025d67bc5d4
Nelson <contact.nelsonic@gmail.com>
2015-08-01 08:17:16 +0100

    adds scraper for repo stars. fixes https://github.com/nelsonic/github-scraper/issues/27

---
+++ test/stars.test.js
@@ -0,0 +1,43 @@
+var test      = require('tape');
+var stars = require('../lib/stars');
+
+test('Scrape undefined profile (error test) ', function(t) {
+	stars(null, function(err){
+		t.ok(err, 400, 'Receive 400 Error when orgname is undefined');
+		t.end();
+	})
+})
+
+test('Scrape random (non-existent) profile (error test) ', function(t){
+	var username = '' + Math.floor(Math.random() * 1000000000000000); // a nice long "random" number
+	stars(username, function(err, data){
+		t.ok(err === 404, 'Got 404 Error when username does not exist');
+		t.ok(typeof data === 'undefined', '@param profile is undefined (as expected)');
+		t.end();
+	})
+})
+
+test('read list of stars for pandajs/sad ', function(t){
+  var username = 'pandajs/sad';
+	stars(username, function(err, data) {
+		// t.ok(data.repos.length === 20, 'first page of org has 20 repos: '+data.repos.length)
+		t.ok(data.stars.length > 0, '"stars": '+data.stars.length);
+		t.ok(typeof data.next === 'undefined', username +' only has 1 page of stars');
+		t.end();
+	});
+})
+
+test('read list of stars for nelsonic/practical-js-tdd (multi-page)', function(t){
+  var username = 'nelsonic/practical-js-tdd';
+	stars(username, function(err, data) {
+		// t.ok(data.repos.length === 20, 'first page of org has 20 repos: '+data.repos.length)
+		t.ok(data.stars.length > 50, '"stars": '+data.stars.length);
+		t.ok(data.next === 'https://github.com/'+username +'/stargazers?page=2', username +' multi-page stars');
+    // crawl second page:
+    stars(data.next, function(err2, data2){
+      t.ok(data2.stars.length > 50, '"stars": '+data.stars.length);
+      t.ok(data2.next === 'https://github.com/'+username +'/stargazers?page=3', username +' multi-page stars');
+		  t.end();
+    })
+	});
+})
--- try.js
+++ try.js
@@ -67,13 +67,23 @@ var repo = 'dwyl/tudo'
 //   })
 // });
 
-var username = 'Marak';
-var following = require('./lib/following');
-following(username, function(err, data){
-  console.log(data);
-  console.log(data.following.length)
-  following(data.next, function(err2, data2){
-    console.log(data2.following.length, data2.next);
+// var username = 'Marak';
+// var following = require('./lib/following');
+// following(username, function(err, data){
+//   console.log(data);
+//   console.log(data.following.length)
+//   following(data.next, function(err2, data2){
+//     console.log(data2.following.length, data2.next);
+//
+//   })
+// });
 
-  })
+var repo = 'nelsonic/practical-js-tdd';
+var stars = require('./lib/stars');
+stars(repo, function(err, data){
+  console.log(data);
+  console.log(data.stars.length)
+  // stars(data.next, function(err2, data2){
+  //   console.log(data2.starers.length, data2.next);
+  // })
 });
