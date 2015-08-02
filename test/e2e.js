e555b6737f973cc9d155a1323b65e825788f5642
Nelson <contact.nelsonic@gmail.com>
2015-08-02 21:18:06 +0100

    adds feed test to e2e https://github.com/nelsonic/github-scraper/issues/38

--- test/e2e.js
+++ test/e2e.js
@@ -12,75 +12,14 @@ test('Scrape a known profile @alanshaw', function(t){
   })
 })
 
-// feed('nelsonic', function(err, list){
-//   console.log(err);
-//   console.log(list, list.entries.length)
-// })
-
-/*
-var test = require('tape');
-var db = require('../src/save.js');
-var F = require('../src/follow.js');
-// var P = require('../src/profile.js');
-var C = require('../src/index.js');
-
-test('E2E with including unfollowing, updating & deleting', function (t) {
-  var user = 'nodecoder';
-  // this is my before function:
-  db.erase(user, function(err4, data){
-    console.log(user + " ✓ erased")
-  });
-
-  C.crawlUser(user, function(err1, profile){
-    // manually remove a follower:
-    var f = Object.keys(profile['followers']);
-    var removed = f[0];
-    f = F.tidyArray(f, f[0])
-    profile = F.updateUsers('followers', profile, f);
-
-    // maunally update, read & delete the record
-    db.save(user, profile, function(err2, log) {
-      db.open(user, function(err3, data) {
-        var profile = JSON.parse(data);
-        // t.equal(profile.followers[removed].length, 2, "✓ "+removed +' unfollowed');
-        // delete user after checking to ensure we throw dberr
-        db.erase(user, function(err4, data){
-          t.end();
-        }); // end erase - ensures we exercise all lines
-      }); // end open
-    }); // end save
-  })
-});
-
-
-test('Test Complete Crawl (and save to db)', function (assert) {
-  var user = 'joaquimserafim';
-  C.crawlUser(user, function (err, profile) {
-    assert.ok(err === null, '✓  no error ' + user);
-    var followers = Object.keys(profile.followers);
-    assert.true(followers.length > 20, "✓ " +user + ' has more than 20 followers');
-    assert.end();
-  });
-});
-
-
-test('Delete record to ensure dberr', function (assert) {
-  var user = 'intool';
-  db.erase(user, function(err){
-    C.crawlUser(user, function (err, profile) {
-      assert.ok(err === null, ' no error ' + user);
-      assert.end();
-    });
-  })
-});
-
-
-// test for failure (user doesn't exist HTTP status = 404)
-test('Test for a non-existant user', function (t) {
-  var user = Math.floor(Math.random() * 1000000000000000); // a nice long "random" number
-  C.crawlUser(user, function (e, p) {
-    t.equal(e, 404, '- 404 for unknown user @' + user);
-    t.end();
-  });
-});
-*/
+test('parse @iteles activity feed (expect recent activity)', function(t){
+	var user = 'iteles';
+	gs.feed(user, function(err, data){
+		t.ok(err === null, 'No error when parsing @' +user +' activity feed');
+    var entry = data.entries.filter(function(e){
+      return e.indexOf('commented');
+    })
+    t.ok(data.entries.length === 30, '@' +user +' activity feed contains 30 entries')
+		t.end();
+	})
+})
