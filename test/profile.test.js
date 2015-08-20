ee2487ab662fa545e513d8553e0f4b99da20e8cb
Nelson <contact.nelsonic@gmail.com>
2015-08-20 12:33:55 +0100

    @nelsonic updated his website without warning us! how dare he! #iBrokeTheDam

--- test/profile.test.js
+++ test/profile.test.js
@@ -9,7 +9,7 @@ test('Scrape @nelsonic GitHub profile (consistent state profile)', function(t){
 		t.ok(data.joined   === '2010-02-02T08:44:49Z', '- @' + user + ' Joined Date 2009-01-20T10:54:20Z has not chaged');
 		t.ok(data.worksfor === 'dwyl.io', '- @' + user + ' Works for dwyl');
 		t.ok(data.email    === 'contact.nelsonic@gmail.com', '- @' + user + ' Email address is: contact.nelsonic@gmail.com');
-		t.ok(data.website  === 'http://linkedin.com/in/nelsonic', '- @' + user + ' Website URL is linkedin');
+		t.ok(data.website  === 'http://www.dwyl.io/', '- @' + user + ' Website URL is linkedin');
 		t.ok(data.location === 'London', '- @' + user + ' Based in London');
 		t.ok(data.followercount > 100, '- @' + user + ' Has more than 100 followers');
 		t.ok(data.starred > 100, '- @' + user + ' Has starred '+ data.starred);
