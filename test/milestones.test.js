ef6297acf36b3fb12cf390e97a0ec4de4ec2f5ea
nelsonic <contact.nelsonic+github@gmail.com>
2016-03-26 12:44:22 +0000

    update DOM selector for repo > programmingLanguage

--- test/milestones.test.js
+++ test/milestones.test.js
@@ -1,7 +1,7 @@
 var test       = require('tape');
 var milestones = require('../lib/switcher');
 
-test.only('crawl /dwyl/tudo/milestones', function(t){
+test('crawl /dwyl/tudo/milestones', function(t){
 	var project = '/dwyl/tudo/milestones';
 	milestones(project, function(err, data) {
     console.log(data);
