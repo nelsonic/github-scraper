873370f009d7d3420dfe466cc24bf57eebbbecfb
nelsonic <contact.nelsonic+github@gmail.com>
2017-05-28 09:47:51 +0100

    org > repo > stargazers recursive script works. runs too fast. must slow it down!

--- examples/list-repos.js
+++ examples/list-repos.js
@@ -1,4 +1,19 @@
+var gs = require('../lib');
 var stars = require('./stars-recursive-scrape-save.js')
-// run first time
-var repo = 'dwyl/learn-tdd'
-stars(repo);
+
+var org = 'dwyl/';
+gs(org, process_org_page);
+
+function process_org_page(err, data) {
+  if(data && data.entries) {
+    data.entries.forEach(function (repo) {
+      stars(org + repo.name);
+      if(data.next_page) {
+        // gs(data.next_page, process_org_page);
+      }
+    })
+  }
+  else {
+    console.log(data);
+  }
+}
