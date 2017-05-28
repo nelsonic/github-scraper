0e9c98d4209223e355125decc20949e93d00ca1c
nelsonic <contact.nelsonic+github@gmail.com>
2017-05-28 09:32:29 +0100

    fixed org (list of repos) scraper re-enables failing tests. :tada: #73

--- test/org.test.js
+++ test/org.test.js
@@ -4,16 +4,17 @@ var org  = require('../lib/switcher');
 test('Scrape an org WITHOUT a next page (known data)', function(t){
 	var url = '/peersun';
 	org(url, function(err, data) {
-		console.log(data);
-		t.ok(data.entries.length === 5, 'org '+url + ' has ' +data.entries.length + ' repos.')
+		// console.log(data);
+		t.ok(data.entries.length > 5, 'org '+url + ' has ' +data.entries.length + ' repos.')
 		t.ok(data.pcount === 0, '"pcount":'+data.pcount);
 		t.end();
 	});
 })
 
-test.skip('Scrape an org WITH a next page', function(t){
+test('Scrape an org WITH a next page', function(t){
 	var url = '/github';
 	org(url, function(err, data) {
+		// console.log(data);
 		t.ok(data.pcount > 100, '"pcount":'+data.pcount);
 		t.ok(data.location === 'San Francisco, CA', 'data.location: '+data.location);
 		t.ok(data.website === 'https://github.com/about', 'data.url: '+data.url);
@@ -22,18 +23,18 @@ test.skip('Scrape an org WITH a next page', function(t){
 	});
 })
 
-test.skip('Fetch Second page of dwyl org', function(t){
+test('Fetch Second page of dwyl org', function(t){
 	var url = '/dwyl?page=2';
 	org(url, function(err, data) {
 		// console.log(data);
-		t.ok(data.entries.length === 20, 'SECOND page of org has 20 repos: '+data.entries.length)
+		t.ok(data.entries.length === 30, 'SECOND page of org has 20 repos: '+data.entries.length)
 		t.ok(data.pcount > 10, '"pcount":'+data.pcount);
 		t.ok(data.next_page === '/dwyl?page=3', 'dwyl has more than one page');
 		t.end();
 	});
 })
 
-test.skip('ORG with no people', function(t){
+test('ORG with no people', function(t){
 	var url = '/pandajs';
 	org(url, function(err, data) {
 		t.ok(typeof data.website === 'undefined', "No website")
