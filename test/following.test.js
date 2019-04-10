5e656c0dee7c8976c5c101b3e982e2534b655b57
nelsonic <contact.nelsonic+github@gmail.com>
2019-04-10 14:54:49 +0100

    update test/following.test.js to conform to the new next_page format https://github.com/nelsonic/github-scraper/issues/91#issuecomment-481695236

--- test/following.test.js
+++ test/following.test.js
@@ -1,26 +1,27 @@
 var test      = require('tape');
 var following = require('../lib/switcher');
 
-test('read list of following for @iteles ', function(t){
-  var url = 'iteles/following';
-	following(url, function(err, data) {
-		// t.ok(data.repos.length === 20, 'first page of org has 20 repos: '+data.repos.length)
-		t.ok(data.entries.length > 10, '"following": '+data.entries.length);
-		t.ok(typeof data.next === 'undefined', url +' only has 1 page of following');
+test('read list of following for @torvalds (zero people!)', function(t){
+  var url = 'torvalds/following';
+	following(url, function (err, data) {
+		t.ok(data.entries.length === 0, '"following": '+data.entries.length);
+		t.ok(typeof data.next_page === 'undefined', url
+      +' has no "next_page" because he is not following anyone!');
 		t.end();
 	});
 })
 
 test('read list of following for @Marak (multi-page)', function(t){
   var url = 'Marak/following';
-	following(url, function(err, data) {
-		// t.ok(data.repos.length === 20, 'first page of org has 20 repos: '+data.repos.length)
+	following(url, function (err, data) {
 		t.ok(data.entries.length > 50, '"following": '+data.entries.length);
-		t.ok(data.next_page === 'https://github.com/'+url +'?page=2', url +' multi-page following');
+    t.ok(data.next_page.indexOf(url + '?after=') > -1,
+      url +' multi-page followers');
     // crawl second page:
-    following(data.next_page, function(err2, data2){
+    following(data.next_page, function (err2, data2) {
       t.ok(data2.entries.length > 50, '"following": '+data.entries.length);
-      t.ok(data2.next_page === 'https://github.com/'+url +'?page=3', url +' multi-page following');
+      t.ok(data.next_page.indexOf(url + '?after=') > -1,
+        url +' multi-page followers');
 		  t.end();
     })
 	});
