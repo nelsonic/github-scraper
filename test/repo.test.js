b68fd200ccbf87c51bc17f94435d15c0e9a1c3f2
nelsonic <nelson+github@dwyl.io>
2019-04-12 19:09:46 +0100

    fix repo description and website DOM selector #96

--- test/repo.test.js
+++ test/repo.test.js
@@ -4,7 +4,7 @@ var repo = require('../lib/switcher');
 test('crawl known repository for stats', function(t) {
 	var project = 'dwyl/adoro';
 	repo(project, function(err, stats) {
-		// console.log(stats);
+		console.log(stats);
 		t.equal(stats.type, 'repo', project + ' data.type: ' + stats.type);
 		t.ok(err === null, 'No Error when crawling ' + project +' (repo pages)');
 		t.ok(stats.watchers > 3, ' has more than 1 watchers: '+stats.watchers);
@@ -36,6 +36,24 @@ test('crawl forked repo', function(t){
   var project = '/backhand/github-scraper';
   repo(project, function(err, stats) {
     t.ok(stats.forkedfrom === '/nelsonic/github-scraper', 'Repo forked from /nelsonic/github-scraper')
+    t.end();
+  })
+})
+
+test('crawl /dwyl/start-here (known repo)', function(t){
+  var project = '/dwyl/start-here';
+  repo(project, function(err, stats) {
+		t.ok(stats.description.indexOf('Quick-start Guide') > -1, 
+			project + ' description: ' + stats.description);
+    t.end();
+  })
+})
+
+test('dwyl/todo-list-javascript-tutorial known website', function(t){
+  var project = 'dwyl/todo-list-javascript-tutorial';
+  repo(project, function(err, stats) {
+		t.ok(stats.website === 'https://todomvc-app.herokuapp.com', 
+			project + ' website: ' + stats.website);
     t.end();
   })
 })
