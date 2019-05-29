e6c4df46330ba74edad67eabe0f06ee7f92c562a
nelsonic <nelson+github@dwyl.io>
2019-05-29 09:00:46 +0100

    [WiP] revive issue parser for #103

--- test/issue.test.js
+++ test/issue.test.js
@@ -1,7 +1,7 @@
 var test  = require('tape');
 var issue = require('../lib/switcher');
 
-test.skip('Scrape /dwyl/tudo/issues/51 for all comments and meta-data', function(t){
+test.only('Scrape /dwyl/tudo/issues/51 for comments & meta-data', function (t) {
 	var url = '/dwyl/tudo/issues/51';
 	issue(url, function(err, data) {
 		t.ok(data.url.indexOf(url) > -1, url + ' is: ' +data.url)
@@ -11,12 +11,16 @@ test.skip('Scrape /dwyl/tudo/issues/51 for all comments and meta-data', function
 		t.ok(data.created.length > 0, url + ' was created on: '+data.created);
 		// labels
 		t.ok(data.labels.length > 2, url + ' has '+data.labels.length + ' labels')
-		t.ok(data.milestone === 'Minimal Usable Product', 'Milestone is: '+data.milestone);
+		t.ok(data.milestone === 'Minimal Usable Product', 'Milestone is: '
+			+ data.milestone);
 		t.ok(data.assignee.length > 0, url + ' has assignee: '+ data.assignee);
-		t.ok(data.participants.length > 2, url + ' has participants: ' + data.participants);
-		t.ok(data.participants.indexOf('iteles') > -1, url + ' has participation from @iteles');
+		t.ok(data.participants.length > 2, url + ' has participants: '
+			+ data.participants);
+		t.ok(data.participants.indexOf('iteles') > -1, url
+			+ ' has participation from @iteles');
 
-		t.ok(data.entries.length > 2, url + ' has: '+data.entries.length);
+		t.ok(data.entries.length > 2,
+				url + ' has: '+data.entries.length + ' comments');
 
 		t.end();
 	});
