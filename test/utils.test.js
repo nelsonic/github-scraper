81c42f4e023545a6612cdb4184169cf123b05fc7
nelsonic <nelson+github@dwyl.io>
2020-04-01 22:01:36 +0100

    add initial tests and implimentation for our parse_int/1 function #108

--- test/utils.test.js
+++ test/utils.test.js
@@ -2,6 +2,9 @@ var test = require('tape');
 var parse_int = require('../lib/utils').parse_int;
 
 test('crawl known repository for stats', function(t) {
-  t.equal(parse_int("1"), 1, "1")
+  t.equal(parse_int("1"), 1, '"1" => 1')
+  t.equal(parse_int("  1  "), 1, '"  1  " => 1')
+  t.equal(parse_int("300"), 300, '"300" => 300')
+  t.equal(parse_int("4.3k"), 4300, '"4.3k" => 4300')
   t.end()
 })
