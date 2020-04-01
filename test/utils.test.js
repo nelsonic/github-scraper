ced3e8420ff753ea2ccb9a70dc856931297631dd
nelsonic <nelson+github@dwyl.io>
2020-04-01 22:21:08 +0100

    future proof parse_int to allow for *MILLIONS* of stars! https://github.com/nelsonic/github-scraper/issues/108#issuecomment-606524065 https://youtu.be/EJR1H5tf5wE?t=14

--- test/utils.test.js
+++ test/utils.test.js
@@ -1,10 +1,16 @@
 var test = require('tape');
 var parse_int = require('../lib/utils').parse_int;
 
-test('crawl known repository for stats', function(t) {
+test('Parse Integers from repo stats Strings', function(t) {
   t.equal(parse_int("1"), 1, '"1" => 1')
   t.equal(parse_int("  1  "), 1, '"  1  " => 1')
   t.equal(parse_int("300"), 300, '"300" => 300')
+  t.equal(parse_int("1k"), 1000, '"1k" => 1000')
   t.equal(parse_int("4.3k"), 4300, '"4.3k" => 4300')
+  t.equal(parse_int("89.6k"), 89600, '"89.6k" => 89600')
+  t.equal(parse_int("146k"), 146000, '"146k" => 146000')
+  t.equal(parse_int("310k"), 310000, '"310k" => 310000')
+  t.equal(parse_int("1m"), 1000000, '"1m" => 1000000')
+  t.equal(parse_int("1.1m"), 1100000, '"1.1m" => 1100000')
   t.end()
 })
