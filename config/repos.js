b5e843ea49d8459379461e61bc7213aab8345f55
Abdelhak Akermi <abdelhak.akermi@gmail.com>
2020-07-23 17:52:19 +0300

    add constant selectors

---
+++ config/repos.js
@@ -0,0 +1,10 @@
+
+
+const SELECTORS={
+    COMMIT:".Box-header--blue strong",
+    LANGUAGES:".BorderGrid--spacious .BorderGrid-row",
+    FORKED_FROM:'a[data-hovercard-type="repository"]',
+    TOPIC_TAG:".topic-tag"
+}
+
+module.exports = SELECTORS;
