f0743303f7d00f96b7e6bd5eb27e6e42b898a5a5
Abdelhak Akermi <abdelhak.akermi@gmail.com>
2020-07-24 13:16:52 +0300

    fix followers

--- config/repos.js
+++ config/repos.js
@@ -4,6 +4,7 @@ const SELECTORS={
     COMMIT:".Box-header--blue strong",
     LANGUAGES:".BorderGrid--spacious .BorderGrid-row",
     FORKED_FROM:'a[data-hovercard-type="repository"]',
+    FOLLOWERS:'.border-gray-light',
     TOPIC_TAG:".topic-tag"
 }
 
