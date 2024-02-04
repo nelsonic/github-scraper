5e6c3f173faf24dbb86e7fa8244867af633fbccd
nelsonic <nelson+github@dwyl.com>
2024-02-04 01:30:04 +0000

    debug(context) #128

--- lambda/index.js
+++ lambda/index.js
@@ -1,7 +1,8 @@
 require("env2")(".env");
-const debug = require("./lib/debug.js");
+const debug = require("./debug.js");
 
 exports.handler = function handler (event, context, callback) {
   debug(event);
+  debug(context);
   return callback(null, event);
 }
