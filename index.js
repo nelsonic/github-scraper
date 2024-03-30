f28d061340b26990b846a8b8c1a6d5fa412b1c05
nelsonic <nelson+github@dwyl.com>
2024-03-30 01:43:51 +0000

    log rawPath #128

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ exports.handler = function handler (event, context, callback) {
   console.log(event);
   console.log("Hi Friends!")
   debug(event);
+  console.log('rawPath:', event.rawPath)
 
   const url = 'dwyl';
   gs(url, function(err, data) {
