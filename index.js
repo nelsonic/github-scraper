cef3903d9cc59b3db94c8c870b56faf83bebe5d9
nelsonic <nelson+github@dwyl.com>
2024-03-30 01:06:14 +0000

    run gs in lambda function #128

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 require("env2")(".env");
 const debug = require("./lambda/debug.js");
+const gs = require('./lib/');
 
 exports.handler = function handler (event, context, callback) {
   console.log(event);
+  console.log("Hi Friends!")
   debug(event);
-  return callback(null, event);
+
+  const url = 'dwyl';
+  gs(url, function(err, data) {
+    console.log(' - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - ')
+    console.log(data);
+  
+    return callback(null, event);
+  });
 }
